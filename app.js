31c39230b372e097781387faad2c4d7e2d2d498d
weishengit <44973330+weishengit@users.noreply.github.com>
2020-09-17 15:33:13 +0800

    Added Correct Files

--- app.js
+++ app.js
@@ -1,52 +1,84 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-    let temp_desc = document.querySelector('.temperature-description');
-    let temp_degree = document.querySelector('.temperature-degrees');
-    let location_timezone = document.querySelector('.location-timezone');
-    let temp_section = document.querySelector('.degree-section');
-    const temp_span = document.querySelector('.degree-section span');
-    
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position =>{
-            
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+const holidayList = document.getElementById('holidayList');
+const searchBar = document.getElementById('searchBar');
+let phHolidays = [];
 
-            const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&
-            exclude=minutely,hourly,daily&appid=5c375abe61fc08ba4046affd5734f9e8`;
-
-            fetch(api)
-            .then(responce => {
-                return responce.json();
-            })
-            .then(data => {
-                console.log(data);
-                const {temp, weather} = data.current;
-                const {description, icon} = data.current.weather[0];
-                let degree_celcius = (temp - 273.15);
-
-                //Set DOM elements from the API
-                temp_degree.textContent = parseFloat(degree_celcius).toFixed(2);
-                temp_desc.textContent = description;
-                location_timezone.textContent = data.timezone;
-                document.getElementById('icon').innerHTML='<img src=\'http://openweathermap.org/img/wn/' + icon +'@2x.png\'>'
-
-                //Measurement Selector
-                temp_section.addEventListener('click', ()=> {
-                    if(temp_span.textContent === 'C'){
-                        temp_span.textContent = 'F';
-                        let degree_farh = (degree_celcius * 9/5) + 32;
-                        temp_degree.textContent = parseFloat(degree_farh).toFixed(2);
-                    } else {
-                        temp_span.textContent = 'C';
-                        temp_degree.textContent = parseFloat(degree_celcius).toFixed(2);
-                    }
-                });
-            });
-        });
-    } else {
-        h1.textContent = 'Unable To Find Current Location'
-    }
-
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredHolidays = phHolidays.holidays.filter((holidays) => {
+        return(holidays.name.toLowerCase().includes(searchString))
+    });
+    displayHolidays(filteredHolidays);
 });
+
+
+
+const loadHolidays = async () => {
+    try {
+        const res = await fetch('https://holidayapi.com/v1/holidays?pretty&country=PH&year=2019&key=b293a1d9-10e3-48c9-ba1b-6e5dccfa93ca');
+        phHolidays = await res.json();
+        displayHolidays(phHolidays.holidays);
+        console.log(phHolidays.holidays);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayHolidays = (holidays) => {
+    const htmlString = holidays
+        .map((holiday) => {
+            let cDate = holiday.date;
+            let mon = cDate.substr(5, 2);
+            let day = cDate.substr(8, 2);
+            switch(mon){
+                case "01":
+                    mon = "January"
+                    break;
+                case "02":
+                    mon = "February"
+                    break;
+                case "03":
+                    mon = "March"
+                    break;
+                case "04":
+                    mon = "April"
+                    break;
+                case "05":
+                    mon = "May"
+                    break;
+                case "06":
+                    mon = "June"
+                    break;
+                case "07":
+                    mon = "July"
+                    break;
+                case "08":
+                    mon = "August"
+                    break;
+                case "09":
+                    mon = "September"
+                    break;
+                case "10":
+                    mon = "October"
+                    break;
+                case "11":
+                    mon = "November"
+                    break;
+                case "12":
+                    mon = "December"
+                    break;
+                default:
+                    mon = "None"
+                    break;
+            };
+            return `
+            <li class="holiday">
+                <h2>${holiday.name}</h2>
+                <p>Date: ${mon} ${day}</p>
+            </li>
+        `;
+        })
+        .join('');
+    holidayList.innerHTML = htmlString;
+};
+
+loadHolidays();
